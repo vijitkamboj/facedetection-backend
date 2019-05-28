42e0132594a3cf8cdea2c30777edcb5e0fabe86d
Vijit Kamboj <vijitkamboj92@gmail.com>
2019-05-28 16:07:53 +0530

    connected postgresql to the server

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const knex = require('knex');
+
+const Database = knex({
+    client : 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: '43215',
+        database: 'facedetection'
+    }
+})
+
+console.log(Database.select('*').from('users') )
 
 const app = express();
 
