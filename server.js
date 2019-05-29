a81f1bc4662ff95200b4eaf2393a648fe6f93067
Vijit Kamboj <vijitkamboj92@gmail.com>
2019-05-30 01:13:21 +0530

    Deploy

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.put('/imagecount', image.handleImageCount(db))
 
 app.post('/imageApi', image.handleImageApi )
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("App is running at port 3000")
 })
 
