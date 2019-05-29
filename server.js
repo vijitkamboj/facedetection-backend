d5e71ceab0a9aa0e35e60565e3e1b5d1cf29143a
Vijit Kamboj <vijitkamboj92@gmail.com>
2019-05-30 02:15:18 +0530

    disabled cors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt-nodejs')
-const cors = require('cors')
+// const cors = require('cors')
 const knex = require('knex');
 
 const register = require('./controllers/register')
@@ -10,7 +10,7 @@ const image = require('./controllers/image')
 
 const app = express();
 app.use(bodyParser.json())
-app.use(cors())
+// app.use(cors())
 
 const db = knex({
     client: 'pg',
