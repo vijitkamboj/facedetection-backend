b3b55db924fdebdd066792ae218cd8f8818310ad
Vijit Kamboj <vijitkamboj92@gmail.com>
2019-05-31 00:18:46 +0530

    configured cors

--- controllers/signin.js
+++ controllers/signin.js
@@ -9,27 +9,15 @@ const controller = (db,bcrypt) => (req,res) => {
                 return (
                     db('users').select('*').where('email', '=',req.body.email )
                     .then(User => {
-                        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-                        res.setHeader('Access-Control-Allow-Methods', 'POST');
-                        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-                        res.setHeader('Access-Control-Allow-Credentials', true);
                         res.json(User[0])
                     })
                 )
             } else {
-                res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-                res.setHeader('Access-Control-Allow-Methods','POST');
-                res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-                res.setHeader('Access-Control-Allow-Credentials', true);
                 return (res.json(false))
             }
         }
 
     }).catch(err => {
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-        res.setHeader('Access-Control-Allow-Methods','POST');
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-        res.setHeader('Access-Control-Allow-Credentials', true);
         res.status(400).json(false)})
 }
 
